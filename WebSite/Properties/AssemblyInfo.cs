8244123d1a569a4c3ee659755ca1a7d7e8f8b469
Jakub Konecki <jkonecki@gmail.com>
2012-09-20 16:43:39 +0100

    Displaying version number.

--- WebSite/Properties/AssemblyInfo.cs
+++ WebSite/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MvcWebRole1")]
+[assembly: AssemblyTitle("ServiceWare.WebSite")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MvcWebRole1")]
+[assembly: AssemblyCompany("ServiceWare Ltd")]
+[assembly: AssemblyProduct("ServiceWare WebSite")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
